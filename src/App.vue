66338b59bb43bf0de70b5d4fa221c060d4c4853d
Sergei Kozelko <KozelkoS@yandex.ru>
2024-12-19 17:01:07 +0000

    Fetch polestar ids from backend

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { ref } from 'vue'
 import PolestarsList from './components/PolestarsList.vue'
+
+const polestars = ref([])
+fetchPolestars()
+
+async function fetchPolestars() {
+  const polestarsResponse = await fetch('https://stt.rsbat.dev/items')
+  polestars.value = await polestarsResponse.json()
+}
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <PolestarsList :items="[1, 2, 3]" />
+      <PolestarsList :items="polestars" />
     </div>
   </header>
 
