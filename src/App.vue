aba1d1261c45081cfea16b84ff33f583e7225b00
Sergei Kozelko <KozelkoS@yandex.ru>
2024-12-19 18:12:49 +0000

    Make polestars list scrollable

--- src/App.vue
+++ src/App.vue
@@ -14,35 +14,8 @@ async function fetchPolestars() {
 
 <template>
   <header>
-    <div class="wrapper">
-      <PolestarsList :items="polestars" />
-    </div>
+    <PolestarsList :items="polestars"/>
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/components/PolestarsList.vue
+++ src/components/PolestarsList.vue
@@ -4,45 +4,30 @@ defineProps(['items'])
 </script>
 
 <template>
-  <div>
+  <div class="wrapper">
     <h3>Polestars:</h3>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/h">Details</RouterLink>
+      <RouterLink v-for="item in items" :key="item" :to="`/${item}`"> {{ item }}</RouterLink>
+    </nav>
   </div>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/h">Details</RouterLink>
-    <RouterLink v-for="item in items" :key="item" :to="`/${item}`"> {{ item }}</RouterLink>
-  </nav>
 </template>
 
-<style>
-nav {
+<style scoped>
+.wrapper {
+  height: 100vh;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+nav {
+  flex: 1;
+  overflow: scroll;
 }
 
 nav a {
   display: block;
-  padding: 0 1rem;
-}
-
-@media (min-width: 1024px) {
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
