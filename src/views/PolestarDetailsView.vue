8584cf23d62f940b153803dd02e8640c30a9b591
Sergei Kozelko <KozelkoS@yandex.ru>
2025-01-17 17:08:06 +0000

    Load price history for polestar

--- src/views/PolestarDetailsView.vue
+++ src/views/PolestarDetailsView.vue
@@ -1,5 +1,31 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const history = ref([])
+fetchHistory()
+
+async function fetchHistory() {
+  const historyResponse = await fetch('https://stt.rsbat.dev/history?id=' + route.params.id)
+  history.value = await historyResponse.json()
+}
+
+watch(
+  () => route.params.id,
+  () => {
+    history.value = []
+    fetchHistory()
+  },
+)
+</script>
+
 <template>
   <main>
     <h1>Polestar {{ $route.params.id }} details</h1>
+    <template v-for="item in history">
+      {{ item }}
+    </template>
   </main>
 </template>
